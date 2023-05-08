d64e939e1f7f391b8e443aa0999fb08ce1d096b5
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-08 13:49:36 -0500

    progress

--- lua/todo-quickfix.lua
+++ lua/todo-quickfix.lua
@@ -1,17 +1,22 @@
 local M = {}
 
 M.todo = function()
-  local query_string = '(comment) @comment (#match? @comment "TODO")'
   local parser = require("nvim-treesitter.parsers").get_parser()
-  local query = vim.treesitter.query.parse_query(parser:lang(), query_string)
+  local query_string = '(comment) @comment (#match? @comment "TODO")'
+  local ok query = pcall(
+    vim.treesitter.query.parse_query(parser:lang(), query_string)
+  )
+  if not ok then return end
+
   vim.pretty_print(query)
+
   local qf_list = {}
-  local lnum, col = n:range()
   local tree = parser:parse()[1]
   -- 0 here refers to the current buffer.
   for _, n in query:iter_captures(tree:root(), 0) do
     local text = vim.treesitter.query.get_node_next(n, 0)
     print(text)
+    local lnum, col = n:range()
     table.insert(qf_list, {
       bufnr = vim.api.nvim_get_current_buf(),
       text = text,
