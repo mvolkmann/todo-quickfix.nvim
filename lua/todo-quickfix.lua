13f720a96f5d562c68719364f7bb694b888882ed
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-08 14:30:15 -0500

    it's working!

--- lua/todo-quickfix.lua
+++ lua/todo-quickfix.lua
@@ -1,24 +1,26 @@
-local M = {}
+-- local M = {}
 
--- TODO: I hope this works!
-M.todo = function()
+-- This populates the quickfix list with lines that contain TODO comments.
+-- See the YouTube video https://www.youtube.com/watch?v=PdaObkGazoU.
+-- M.todo = function()
+local function todo_qf()
   local parser = require("nvim-treesitter.parsers").get_parser()
-  -- local query_string = '(comment) @comment (#match? @comment "TODO")'
-  local query_string = '(comment) @comment'
-  local ok query = pcall(
-    vim.treesitter.query.parse(parser:lang(), query_string)
+  local query_string = '((comment) @comment (#match? @comment "TODO"))'
+
+  -- Calling parse in a pcall handles the case where
+  -- the language doesn't support comment nodes.
+  local ok, query = pcall(
+    vim.treesitter.query.parse, parser:lang(), query_string
   )
   if not ok then return end
+ 
+  -- vim.print(query) -- for debugging
 
-  vim.pretty_print(query)
-
-  -- Will it find comments containing TODO?
   local qf_list = {}
   local tree = parser:parse()[1]
   -- 0 here refers to the current buffer.
   for _, n in query:iter_captures(tree:root(), 0) do
-    local text = vim.treesitter.query.get_node_next(n, 0)
-    print(text)
+    local text = vim.treesitter.query.get_node_text(n, 0)
     local lnum, col = n:range()
     table.insert(qf_list, {
       bufnr = vim.api.nvim_get_current_buf(),
@@ -31,4 +33,9 @@ M.todo = function()
   vim.cmd.copen() -- opens the quickfix list
 end
 
-return M
+-- vim.api.nvim_create_user_command("TodoQF", M.todo, {})
+vim.api.nvim_create_user_command("TodoQF", todo_qf, {})
+
+-- M.todo()
+
+-- return M
